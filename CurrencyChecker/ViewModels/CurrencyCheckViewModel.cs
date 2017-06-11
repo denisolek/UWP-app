8c585df1c76a410bed593cd2a9cb4341dddf69fb
denisolek <denis.olek@gmail.com>
2017-06-11 18:59:30 +0200

    ParseToCurrencyList moved back to CurrencyCheckViewModel

--- CurrencyChecker/ViewModels/CurrencyCheckViewModel.cs
+++ CurrencyChecker/ViewModels/CurrencyCheckViewModel.cs
@@ -13,7 +13,21 @@ namespace CurrencyChecker.ViewModels
 {
     class CurrencyCheckViewModel : ViewModel
     {
-        
+
+        public void ParseToCurrencyList(string json)
+        {
+            dynamic jobject = JsonConvert.DeserializeObject(json);
+
+            CurrencyList = new ObservableCollection<Currency>();
+
+            foreach (var item in jobject.rates)
+            {
+                var testName = item.Name;
+                var testValue = item.Value;
+                CurrencyList.Add(new Currency { Name = testName, Rate = testValue });
+            }
+        }
+
         public async void UpdateCurrencyList()
         {
             string apiResponse = await GetCurrencyList();
--- CurrencyChecker/ViewModels/ViewModel.cs
+++ CurrencyChecker/ViewModels/ViewModel.cs
@@ -16,9 +16,6 @@ namespace CurrencyChecker.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        // This method is called by the Set accessor of each property.
-        // The CallerMemberName attribute that is applied to the optional propertyName
-        // parameter causes the property name of the caller to be substituted as an argument.
         public void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
             if (PropertyChanged != null)
@@ -68,20 +65,6 @@ namespace CurrencyChecker.ViewModels
             Task<string> getStringTask = client.GetStringAsync("http://api.fixer.io/latest?base=" + ChoosenCurrency);
             string urlContents = await getStringTask;
             return urlContents;
-        }
-
-        public void ParseToCurrencyList(string json)
-        {
-            dynamic jobject = JsonConvert.DeserializeObject(json);
-
-            CurrencyList = new ObservableCollection<Currency>();
-
-            foreach (var item in jobject.rates)
-            {
-                var testName = item.Name;
-                var testValue = item.Value;
-                CurrencyList.Add(new Currency { Name = testName, Rate = testValue });
-            }
         }
     }
 }
